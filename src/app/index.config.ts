ffd66c35f39cbcc1e24a9bbadb2952ee52b26b6d
doronzzz <gourdoron@gmail.com>
2016-08-30 14:42:22 +0300

    Update index.config.ts

--- src/app/index.config.ts
+++ src/app/index.config.ts
@@ -3,6 +3,13 @@ export function config($logProvider: angular.ILogProvider, toastrConfig: any) {
   // enable log
   $logProvider.debugEnabled(true);
   // set options third-party lib
+  
+  //DORON: use an object
+  //DORON: toastrConfig = {
+    //timeout:3000,
+    //allowHtml: true
+  //}
+  
   toastrConfig.allowHtml = true;
   toastrConfig.timeOut = 3000;
   toastrConfig.positionClass = 'toast-top-right';
